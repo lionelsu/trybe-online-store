0464167c7e5364fa49f82c920f805b76f6cfd299
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-04-19 18:07:45 -0300

    3º requisito concluido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 export class App extends Component {
   render() {
     return (
       <main>
+
+        <Switch>
+          <Route exact path="/cart" component={ Cart } />
+        </Switch>
+
         <Search />
       </main>
     );
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Input from '../components/Input';
 
 class Search extends Component {
@@ -20,6 +21,16 @@ class Search extends Component {
           >
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>)}
+
+        <Link to="/cart">
+          <button
+            type="button"
+            data-testid="shopping-cart-button"
+          >
+            Ir para o carrinho.
+          </button>
+        </Link>
+
       </section>
     );
   }
