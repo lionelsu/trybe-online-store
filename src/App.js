e3dfd4340a57f84507f145a149cb0e4a2d378af0
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-04-19 18:44:19 -0300

    4º requisito concluido

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import Search from './pages/Search';
 import Cart from './pages/Cart';
+import './App.css';
 
 export class App extends Component {
   render() {
