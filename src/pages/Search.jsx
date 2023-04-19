515877bbad65f1c18af0c804b9ca624f8cb01dd4
Paulo Victor Belens Moreira <paulovictor55296@gmail.com>
2023-04-19 20:00:36 -0300

    5º requisito concluido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Input from '../components/Input';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Search extends Component {
   state = {
     search: '',
     categories: [],
+    searchProduct: [],
+    isProduct: true,
   };
 
   componentDidMount() {
@@ -19,31 +21,51 @@ class Search extends Component {
     this.setState({ categories: result });
   };
 
+  searchProduct = async () => {
+    const { search } = this.state;
+    const result = await getProductsFromCategoryAndQuery(search);
+    const realResult = result.results;
+    if (realResult.length === 0) {
+      this.setState({ isProduct: false });
+    }
+    this.setState({ searchProduct: realResult });
+  };
+
   handlerChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
   };
 
   render() {
-    const { search, categories } = this.state;
+    const { search, categories, searchProduct, isProduct } = this.state;
     return (
       <section>
-        <Input
-          placeholder="Search"
-          type="text"
-          name="search"
-          value={ search }
-          onChange={ this.handlerChange }
-        />
+        <div>
+          <Input
+            placeholder="Search"
+            type="text"
+            name="search"
+            value={ search }
+            onChange={ this.handlerChange }
+            test="query-input"
+          />
+          <button
+            type="button"
+            onClick={ this.searchProduct }
+            data-testid="query-button"
+          >
+            Pesquisar
+          </button>
+        </div>
         <p
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <section className="categories">
-          {categories.map((categoria, index) => (
+          {categories.map((categoria) => (
             <button
               type="button"
-              key={ index }
+              key={ categoria.id }
               data-testid="category"
               className="button__categorie"
             >
@@ -51,7 +73,23 @@ class Search extends Component {
             </button>
           ))}
         </section>
+        <section>
+          {isProduct ? (
+            searchProduct.map((product, index) => (
+              <div
+                key={ index }
+                data-testid="product"
+              >
+                <p>{product.title}</p>
+                <p>{product.price}</p>
+                <img src={ product.thumbnail } alt={ product.title } />
+              </div>
+            ))
+          ) : (
+            <p>Nenhum produto foi encontrado</p>
+          )}
 
+        </section>
         <Link to="/cart">
           <button
             type="button"
